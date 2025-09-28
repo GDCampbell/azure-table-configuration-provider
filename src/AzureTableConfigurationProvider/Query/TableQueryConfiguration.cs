ce3ad22fd063c35f54ba65ce256ca7230bfed236
Gavin Campbell <gavinduncancampbell@gmail.com>
2025-09-28 03:45:53 +0100

    Added configuration model to be used when performing the query to the Azure table.

---
+++ src/AzureTableConfigurationProvider/Query/TableQueryConfiguration.cs
@@ -0,0 +1,23 @@
+﻿using Azure.Data.Tables;
+using System.Linq.Expressions;
+
+namespace AzureTable.Provider.Query;
+
+public sealed class TableQueryConfiguration
+{
+    public IFilter? Filter { get; set; }
+    public int? MaxPerPage { get; set; }
+    public IEnumerable<string>? Select { get; set; }
+}
+
+public interface IFilter;
+
+public sealed class StringFilter(string filter) : IFilter
+{
+    public string Filter { get; } = filter;
+}
+
+public sealed class PredicateFilter<TEntity>(Expression<Func<TEntity, bool>> filter) : IFilter where TEntity : class, ITableEntity
+{
+    public Expression<Func<TEntity, bool>> Filter { get; } = filter;
+}
