3f47332b379954261ac1c5a5569e4d82a4ffe7de
Gavin Campbell <gavinduncancampbell@gmail.com>
2025-09-28 03:49:34 +0100

    Added configuration model and respective builder to enable the user to configure how the configuration source and provider will be created. Includes a Func delegate to receive an IConfiguration and return a TableClient; an Action delegate to configure the mapper builder; and an Action delegate to configure some arguments to do with the table query.

---
+++ src/AzureTableConfigurationProvider/Configuration/ProviderConfigurationBuilder.cs
@@ -0,0 +1,105 @@
+﻿using Azure.Data.Tables;
+using AzureTable.Provider.Configuration.Mapping;
+using AzureTable.Provider.Query;
+using Microsoft.Extensions.Configuration;
+using System.Text;
+
+namespace AzureTable.Provider.Configuration;
+
+public sealed class ProviderConfigurationBuilder<TEntity> where TEntity : class, ITableEntity
+{
+    private Func<IConfiguration, TableClient>? _tableClientFactory;
+    private Action<ConfigurationMapperBuilder<TEntity>, IConfiguration>? _mappingBuilder;
+    private Action<TableQueryConfiguration>? _queryConfiguration;
+
+    /// <summary>
+    /// Configures the factory function used to create a TableClient instance for the provider.
+    /// </summary>
+    /// <param name="tableClientFactory">A delegate that takes an IConfiguration and returns a TableClient instance. Cannot be null.</param>
+    /// <returns>The current ProviderConfigurationBuilder<TEntity> instance for method chaining.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if the provided tableClientFactory is null.</exception>
+    public ProviderConfigurationBuilder<TEntity> ConfigureTableFactory(Func<IConfiguration, TableClient> tableClientFactory)
+    {
+        ArgumentNullException.ThrowIfNull(tableClientFactory);
+
+        _tableClientFactory = tableClientFactory;
+        return this;
+    }
+
+    /// <summary>
+    /// Configures the entity mapping using the specified mapping builder action.
+    /// </summary>
+    /// <param name="mappingBuilder">An action that receives a configuration mapper builder and the current configuration, allowing custom mapping
+    /// logic to be defined. Cannot be null.</param>
+    /// <returns>The current <see cref="ProviderConfigurationBuilder{TEntity}"/> instance for method chaining.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if the provided mappingBuilder is null.</exception>"
+    public ProviderConfigurationBuilder<TEntity> ConfigureMapping(Action<ConfigurationMapperBuilder<TEntity>, IConfiguration> mappingBuilder)
+    {
+        ArgumentNullException.ThrowIfNull(mappingBuilder);
+
+        _mappingBuilder = mappingBuilder;
+        return this;
+    }
+
+    /// <summary>
+    /// Configures query options for the current entity provider using the specified configuration action.
+    /// </summary>
+    /// <param name="queryConfiguration">An action that receives a <see cref="TableQueryConfiguration"/> object to customize query behavior. Cannot be
+    /// null.</param>
+    /// <returns>The current <see cref="ProviderConfigurationBuilder{TEntity}"/> instance for method chaining.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if the provided queryConfiguration is null.</exception>"
+    public ProviderConfigurationBuilder<TEntity> ConfigureQuery(Action<TableQueryConfiguration> queryConfiguration)
+    {
+        // The query configuration is optional but if they're calling this method, then it shouldn't be null.
+        ArgumentNullException.ThrowIfNull(queryConfiguration);
+        _queryConfiguration = queryConfiguration;
+        return this;
+    }
+
+    internal ProviderConfiguration<TEntity> Build()
+    {
+        if (_tableClientFactory is not null && _mappingBuilder is not null)
+        {
+            return new()
+            {
+                MappingBuilder = _mappingBuilder,
+                TableClientFactory = _tableClientFactory,
+                QueryConfiguration = _queryConfiguration
+            };
+        }
+
+        StringBuilder sb = new();
+
+        if (_tableClientFactory is null)
+        {
+            sb.Append(_configureTableFactoryMessage);
+            sb.Append(' ');
+        }
+
+        if (_mappingBuilder is null)
+        {
+            sb.Append(_configureMappingMessage);
+        }
+
+        throw new InvalidOperationException(sb.ToString());
+    }
+
+    private const string _configureTableFactoryMessage = $"{nameof(ProviderConfiguration<TEntity>.TableClientFactory)} is not configured. Please call {nameof(ConfigureTableFactory)}.";
+    private const string _configureMappingMessage = $"{nameof(ProviderConfiguration<TEntity>.MappingBuilder)} is not configured. Please call {nameof(ConfigureMapping)}.";
+}
+
+internal sealed class ProviderConfiguration<TEntity> where TEntity : class, ITableEntity
+{
+    public required Func<IConfiguration, TableClient> TableClientFactory { get; init; }
+    public required Action<ConfigurationMapperBuilder<TEntity>, IConfiguration> MappingBuilder { get; init; }
+    public Action<TableQueryConfiguration>? QueryConfiguration { get; init; }
+
+    public void Deconstruct(out Func<IConfiguration, TableClient> tableClientFactory,
+        out Action<ConfigurationMapperBuilder<TEntity>, IConfiguration> mappingBuilder,
+        out Action<TableQueryConfiguration>? queryConfiguration)
+    {
+        tableClientFactory = TableClientFactory;
+        mappingBuilder = MappingBuilder;
+        queryConfiguration = QueryConfiguration;
+    }
+}
